47bd9d5638bd034ad295d8400c3b20e3ff8037ad
wastal92 <wastal92@github.com>
2018-09-07 21:24:57 +0800

    upload

---
+++ src/MPC.h
@@ -0,0 +1,23 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+class MPC {
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  //Predic the next state before hand to incorporate latency in actuator commands in the model
+  std::vector<double> PredictNextState(std::vector<double> state, std::vector<double> actuations, double latency);
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+};
+
+#endif /* MPC_H */
